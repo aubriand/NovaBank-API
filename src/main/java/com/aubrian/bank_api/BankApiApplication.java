d8c0fa3a93ff517a28aaa7e82e476aa3547c15ee
Aubrian <aubriduhay@gmail.com>
2026-08-11 12:55:40 +0200

    feat: load env variable from .env file

--- src/main/java/com/aubrian/bank_api/BankApiApplication.java
+++ src/main/java/com/aubrian/bank_api/BankApiApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BankApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().systemProperties().load();
 		SpringApplication.run(BankApiApplication.class, args);
 	}
 
